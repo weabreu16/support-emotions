5485510e0f1754b5d355644cf03f87347a01cfb8
William Abreu <weabreu16@gmail.com>
2023-01-05 12:20:39 -0400

    articles page

--- src/pages/article/Article.tsx
+++ src/pages/article/Article.tsx
@@ -1,28 +1,30 @@
 import React from 'react';
 import { Container, Image } from 'react-bootstrap';
 import './ArticleSS.css';
+import Articulo_1 from "../../Images/Articulo_1.png";
+import Articulo_2 from "../../Images/Articulo_2.png";
 
 export const Article = () => {
   return (
     <Container className="mt-2" style={{fontFamily: "'Times New Roman', Times, serif"}}>
-    <h1>Articles</h1>
-    <p className="fs-5 mb-4">In this section we can find the articles published by our experts, with this segment we seek that our specialists feel free to be able to publish their opinions about the topics that are worked on in the area of health and psychology.</p>
+        <h1>Articles</h1>
+        <p className="fs-5 mb-4">In this section we can find the articles published by our experts, with this segment we seek that our specialists feel free to be able to publish their opinions about the topics that are worked on in the area of health and psychology.</p>
 
+        <div className="row">
+            <div className="article1">
+                <Image src={Articulo_1} style={{width: "80%"}} />
+                <h6>Figure 1</h6>
+            </div>
 
-    <div className="row">
-        <div className="article1">
-            <Image src="../../Images/Articulo_1" style={{width: "80%"}} />
-            <h6>Figure 1</h6>
+            <div className="article1">
+                <Image src={Articulo_2} style={{width: "80%"}} />
+                <h6>Figure 2</h6>
+            </div>
         </div>
 
-        <div className="article1">
-            <img src="../../Images/Articulo_2.png" alt="s2" style={{width: "80%"}} />
-            <h6>Figure 2</h6>
-        </div>
-    </div>
 
-
-    <p className="fs-5 mb-4">In figure 1 we can see the different items we can see the following:</p>
+        <p className="fs-5 mb-4">In figure 1 we can see the different items we can see the following:</p>
+        
         <ul>
             <li>Which specialist published the article?</li>
             <li>What date was it published?</li>
